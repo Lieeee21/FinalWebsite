0114bded37709c09a16c8a8c0e1b377469052f88
Lieeee21 <144812284+Lieeee21@users.noreply.github.com>
2024-05-21 18:47:41 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 function toggle(){
-    window.open("http://127.0.0.1:5500/menu.html", "_self")
+    window.location.href="menu.html";
+}
+function toggle1(){
+    window.location.href="order.html";
+}
+function toggle2(){
+    alert("Thank you for being our customer today!")
+}
+function toggle3(){
+    alert("Thank you for contacting us")
 }
 
